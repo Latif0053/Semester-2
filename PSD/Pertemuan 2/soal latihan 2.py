f61fad40fe37280b32356f4b2f0340afa740088a
Latif0053 <aliefmuhammadlatif@gmail.com>
2025-02-26 13:18:53 +0700

    Create soal latihan 2.py

---
+++ PSD/Pertemuan 2/soal latihan 2.py
@@ -0,0 +1,8 @@
+# Soal No.2
+
+A = {1,2,3,4,5}
+B = {4,5,6,7,8}
+
+print("Gabungan himpunan A dan B",A|B)
+print("Irisan himpunan A dan B", A.intersection(B))
+print("Selisih himpunan A dan B" , A - B)
